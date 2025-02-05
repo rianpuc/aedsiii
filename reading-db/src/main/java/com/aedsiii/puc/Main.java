f77a13c23bc0a5f0314650cf96e4f5135da16e64
rianpuc <redlopes@sga.pucminas.br>
2025-02-05 05:03:18 -0300

    testando

--- reading-db/src/main/java/com/aedsiii/puc/Main.java
+++ reading-db/src/main/java/com/aedsiii/puc/Main.java
@@ -3,5 +3,6 @@ package com.aedsiii.puc;
 public class Main {
     public static void main(String[] args) {
         FileParser.parseFile();
+        System.out.println("quero ver se o gitignore funciona");
     }
 }
